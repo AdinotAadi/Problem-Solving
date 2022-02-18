878c71bc96de64e06a2d2c4620616d9bbea77fbe
Aditya Patel <84984325+AdinotAadi@users.noreply.github.com>
2022-02-18 21:44:06 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (71.96%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        boolean isAsc = nums[start] < nums[end];
+        
+        while (start <= end) {
+            int mid = start + ((end - start)/2);
+            if (target == nums[mid]) {
+                return mid;
+            }
+            
+            if (isAsc) {
+                if (target < nums[mid]) {
+                    end = mid -1;
+                }
+                else if (target > nums[mid]) {
+                    start = mid + 1;
+                }
+            }
+            else {
+                if (target > nums[mid]) {
+                    end = mid -1;
+                }
+                else if (target < nums[mid]) {
+                    start = mid + 1;
+                }
+            }
+            
+        }
+        return -1;
+    }
+}
